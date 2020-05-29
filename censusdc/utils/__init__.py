16b0b8a930a823cb517ad798ecfa1684089c3467
Joshua Larsen <jlarsen@usgs.gov>
2020-05-29 13:46:47 -0700

    feat(create_filter): add a filter creation method for CensusTimeSeries and TigerWeb filtering

--- censusdc/utils/__init__.py
+++ censusdc/utils/__init__.py
@@ -1,4 +1,5 @@
-from .utilities import get_wkt_wkid_table, thread_count, RestartableThread
+from .utilities import get_wkt_wkid_table, thread_count, RestartableThread, \
+    create_filter
 from . import geometry
 from .geo import GeoFeatures
 from .servers import TigerWebMapServer, Acs5Server, Acs1Server, Sf3Server
--- censusdc/utils/utilities.py
+++ censusdc/utils/utilities.py
@@ -1,8 +1,33 @@
 import os
 import requests
+import shapefile
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import threading
+import datetime
+
+
+def isnumeric(s):
+    try:
+        float(s)
+        return True
+    except (TypeError, ValueError):
+        return False
+
+
+def isdatetime(s):
+    if isinstance(s, (datetime.date, datetime.datetime)):
+        return True
+    else:
+        return False
+
+
+def isbytes(s):
+    if isinstance(s, bytes):
+        return True
+    else:
+        return False
 
 
 def get_wkt_wkid_table(refresh=False):
@@ -65,3 +90,65 @@ class RestartableThread(threading.Thread):
 
     def clone(self):
         return RestartableThread(self._args, self._kwargs)
+
+
+def create_filter(shp, criteria, return_field):
+    """
+    Filter creation method for large and complex shapefiles
+
+    Parameters
+    ----------
+    shp : shapefile path
+    criteria : dict
+        filter criteria {field_name_0 : [tag], ...,
+                        field_name_n : [tag_0, ..., tag_m]}
+    return_field : str
+        filtered return field name, can be passed to CensusTimeSeries
+        and TigerWeb
+
+    Returns
+    -------
+        tuple
+    """
+    with shapefile.Reader(shp) as foo:
+        header = [i[0].lower() for i in foo.fields[1:]]
+        data = {i: [] for i in header}
+
+        for record in foo.records():
+            for ix, v in enumerate(record):
+                if isnumeric(v):
+                    data[header[ix]].append(v)
+                elif isdatetime(v):
+                    data[header[ix]].append(v)
+                else:
+                    if isbytes(v):
+                        v = v.decode()
+                    data[header[ix]].append(v.lower())
+
+    df = pd.DataFrame.from_dict(data)
+
+    # prep criteria dictionary
+    c2 = {k.lower() : [] for k in criteria.keys()}
+    for k, v in criteria.items():
+        if not isinstance(v, (tuple, list, np.ndarray)):
+            v = [v]
+
+        for i in v:
+            if isnumeric(i):
+                c2[k.lower()].append(i)
+            elif isdatetime(i):
+                c2[k.lower()].append(i)
+            else:
+                c2[k.lower()].append(i.lower())
+
+    for key in c2.keys():
+        if key not in header:
+            err = "{}: is not a valid shapefile attribute name".format(key)
+            raise KeyError(err)
+
+    # intersect criteria with shapefile attribute df
+    for k, v in c2.items():
+        df = df.loc[df[k].isin(v)]
+
+    x = tuple(df[return_field].values)
+    return x
