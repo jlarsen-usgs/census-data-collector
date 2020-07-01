e6dd6ba12a908ff590252419043aff96e4e280f2
Joshua Larsen <jlarsen@usgs.gov>
2020-06-30 17:06:30 -0700

    Added lon_lat_to_albers and albers_to_lon_lat conversions for equal area intersection operations
    Reprojection based on USGS professional paper 1395 (Snyder, 1987).

--- censusdc/utils/geometry.py
+++ censusdc/utils/geometry.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 
+# Constants for lon_lat_to_albers and albers_to_lon_lat
+# Developed from Snyder, 1987. USGS Professional paper 1395
+TO_RAD = np.pi / 180.
+SP1 = 0 * TO_RAD
+SP2 = 60 * TO_RAD
+LON_ORIG = 0 * TO_RAD
+LAT_ORIG = 0 * TO_RAD
+N = 0.5 * (np.sin(SP1) + np.sin(SP2))
+C = np.cos(SP1) ** 2 + (2 * N * np.sin(SP1))
+RHO0 = np.sqrt((C - (2 * N * np.sin(LAT_ORIG)))) / N
+
+
 def calculate_circle(x, y, radius):
     """
     Method to calculate a polygon of radius r around a point
@@ -23,3 +35,85 @@ def calculate_circle(x, y, radius):
     x = x + (np.cos(radians) * radius)
     y = y + (np.sin(radians) * radius)
     return np.array([x, y])
+
+
+def lon_lat_to_albers(lon, lat):
+    """
+    Method to convert decimal longitute and latitude to albers equal
+    area projection
+
+    Developed from Snyder, 1987. USGS Professional paper 1395
+
+    Parameters
+    ----------
+    lon : list or numpy array of longitude
+    lat : list of numpy array of latitude
+
+    Returns
+    -------
+    x, y: tuple
+        tuple of projected coordinates
+
+    """
+    if not isinstance(lon, np.ndarray):
+        lon = np.array(lon, dtype=float)
+
+    if not isinstance(lat, np.ndarray):
+        lat = np.array(lat, dtype=float)
+
+    if lat.shape != lon.shape:
+        raise AssertionError("The shape of the lat and lon array must be "
+                             "exactly the same")
+
+    lat *= TO_RAD
+    lon *= TO_RAD
+
+    RHO = np.sqrt((C - (2 * N * np.sin(lat)))) / N
+    THETA = N * (lon - LON_ORIG)
+
+    x = RHO * np.sin(THETA)
+    y = RHO0 - (RHO * np.cos(THETA))
+
+    # h, scale factor along meridians
+    # k, scale factor along parallels... 1/h
+
+    return x, y
+
+
+def albers_to_lon_lat(x, y):
+    """
+    Method to convert from albers equal area projection back to WGS84
+    lat. lon.
+
+    Developed from Snyder, 1987. USGS Professional paper 1395
+
+    Parameters
+    ----------
+    x : list or numpy array of x points
+    y : list or numpy array of y points
+
+    Returns
+    -------
+        lon, lat: tuple
+    """
+    if not isinstance(x, np.ndarray):
+        lon = np.array(x)
+
+    if not isinstance(y, np.ndarray):
+        lat = np.array(y)
+
+    if x.shape != y.shape:
+        raise AssertionError("The shape of the lat and lon array must be "
+                             "exactly the same")
+
+    # do not covert x, y to radians, because they are already in radial
+    # coordinates
+
+    THETA = np.arctan(x / (RHO0 - y))
+    t = THETA / TO_RAD
+    RHO = np.sqrt(np.square(x) + np.square(RHO0 - y))
+
+    lon = (LON_ORIG + (THETA / N)) / TO_RAD
+    lat = np.arcsin((C - (RHO**2 * N**2)) / (2 * N)) / TO_RAD
+
+    return lon, lat
